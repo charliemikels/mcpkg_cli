8f9e48490826fe3e59c0044d2cc49271365029d4
charliemikels <charliemikels@gmail.com>
2021-12-20 20:24:38 -0500

    Ultra basic example code. Likely won't run on a fresh install.

--- mcpkg_cli.v
+++ mcpkg_cli.v
@@ -3,5 +3,17 @@ module main
 import mcpkg
 
 fn main() {
-	println('Hello World!')
+	mut api := mcpkg.new_api()
+	api.initialize()
+
+	demo_mods := api.search_for_mods(mcpkg.SearchFilter{
+		game_versions:['1.17.1'],
+		query:'sodium',
+		platform_name:'modrinth'
+	})
+
+	if demo_mods.len > 0 {
+		demo_mod := demo_mods[0]
+		api.install_mod(demo_mod)
+	}
 }
